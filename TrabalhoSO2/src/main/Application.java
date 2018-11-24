fac4d430da32ca2024bd3dfbe8fb61cb77b638cd
raviassis <ravi.g.assis@gmail.com>
2018-11-24 20:27:21 -0200

    Criado classe Gestor

--- TrabalhoSO2/src/main/Application.java
+++ TrabalhoSO2/src/main/Application.java
@@ -2,6 +2,7 @@ package main;
 
 import java.util.List;
 
+import gerenciamento.Gestor;
 import gerenciamento.Leitor;
 import gerenciamento.Pedido;
 
@@ -10,8 +11,14 @@ public class Application {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		List<Pedido> pedidos = Leitor.ler("dados.csv");
+		Gestor gestor = new Gestor();
 		
-		pedidos.forEach( p -> System.out.println(p));
+		pedidos.forEach( p -> gestor.inserirPedido(p));
+		
+		while (gestor.temPedido() ) {
+			System.out.println( gestor.pegarPedidoDescendo() );
+		}
+		
 	}
 
 }
