7b02004abe02e5949e531717af8fae719888255c
raviassis <ravi.g.assis@gmail.com>
2018-11-25 16:43:46 -0200

    alterações classe produtor

--- TrabalhoSO2/src/main/Application.java
+++ TrabalhoSO2/src/main/Application.java
@@ -8,21 +8,24 @@ import gerenciamento.GestorFifo;
 import gerenciamento.IGestor;
 import gerenciamento.Leitor;
 import gerenciamento.Pedido;
+import gerenciamento.Produtor;
 import gerenciamento.Sentido;
 
 public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		List<Pedido> pedidos = Leitor.ler("dados.csv");
+		//List<Pedido> pedidos = Leitor.ler("dados.csv");
 		IGestor gestor = new GestorElevator();
 		
-		pedidos.forEach( p -> gestor.inserir(p));
+		Produtor produtor = new Produtor(gestor);
+		
+		/*pedidos.forEach( p -> gestor.inserir(p));
 		
 		System.out.println(gestor.pegar(7, Sentido.PARADO));
 		System.out.println(gestor.pegar(8, Sentido.SUBINDO));
 		System.out.println(gestor.pegar(15, Sentido.DESCENDO));
-		System.out.println(gestor.pegar(0, Sentido.SUBINDO));
+		System.out.println(gestor.pegar(0, Sentido.SUBINDO));*/
 		
 	}
 
